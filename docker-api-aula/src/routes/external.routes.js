9facf58618fc88ce38f6c2bf6d20a0a28df0b7b6
JVGomes6 <joaovitorogomes.profissional@gmail.com>
2026-06-20 01:52:58 -0300

    Implementa CRUD, API, EXTERNAS E DOCUMENTAÇÃO SWAGGER (rotas)

--- docker-api-aula/src/routes/external.routes.js
+++ docker-api-aula/src/routes/external.routes.js
@@ -7,8 +7,65 @@ const {
   getPosts
 } = require('../controllers/external.controller')
 
+
+/**
+ * @swagger
+ * /external/cep/{cep}:
+ *   get:
+ *     summary: Consulta um endereço pelo CEP
+ *     parameters:
+ *       - in: path
+ *         name: cep
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: "01001000"
+ *     responses:
+ *       200:
+ *         description: Endereço encontrado
+ *       400:
+ *         description: CEP inválido ou não encontrado
+ */
+
 router.get('/cep/:cep', getCep)
+
+/**
+ * @swagger
+ * /external/tempo:
+ *   get:
+ *     summary: Consulta o tempo atual
+ *     parameters:
+ *       - in: query
+ *         name: lat
+ *         required: true
+ *         schema:
+ *           type: number
+ *         example: -23.55
+ *       - in: query
+ *         name: lon
+ *         required: true
+ *         schema:
+ *           type: number
+ *         example: -46.63
+ *     responses:
+ *       200:
+ *         description: Dados meteorológicos retornados
+ *       400:
+ *         description: Latitude ou longitude não informadas
+ */
+
 router.get('/tempo', getTempo)
+
+/**
+ * @swagger
+ * /external/posts:
+ *   get:
+ *     summary: Lista posts da API JSONPlaceholder
+ *     responses:
+ *       200:
+ *         description: Lista de posts retornada com sucesso
+ */
+
 router.get('/posts', getPosts)
 
 module.exports = router
--- docker-api-aula/src/routes/task.routes.js
+++ docker-api-aula/src/routes/task.routes.js
@@ -10,11 +10,150 @@ const {
   remove
 } = require('../controllers/task.controller')
 
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: Lista todas as tasks
+ *     responses:
+ *       200:
+ *         description: Lista de tasks retornada com sucesso
+ */
+
 router.get('/',     getAll)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   get:
+ *     summary: Busca uma task pelo ID
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Task encontrada
+ *       404:
+ *         description: Task não encontrada
+ */
+
 router.get('/:id',  getById)
+
+
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Cria uma task
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: Estudar Swagger
+ *               description:
+ *                 type: string
+ *                 example: Documentar a API
+ *               completed:
+ *                 type: boolean
+ *                 example: false
+ *     responses:
+ *       201:
+ *         description: Task criada
+ */
+
 router.post('/',    create)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   put:
+ *     summary: Substitui completamente uma task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               completed:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Task atualizada
+ */
+
 router.put('/:id',  replace)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   patch:
+ *     summary: Atualiza parcialmente uma task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     responses:
+ *       200:
+ *         description: Task atualizada
+ */
+
 router.patch('/:id', update)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   delete:
+ *     summary: Remove uma task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Task removida com sucesso
+ *       404:
+ *         description: Task não encontrada
+ */
+
 router.delete('/:id', remove)
 
 module.exports = router
